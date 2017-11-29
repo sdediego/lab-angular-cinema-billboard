9ed97865797687f5a3a26a791342e2f8ce87b547
sdediego <sergio.dediego@outlook.com>
2017-11-29 18:32:38 +0100

    Iteration 2

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { routes } from './routes';
 
 import { AppComponent } from './app.component';
+import { MyHomeComponent } from './my-home/my-home.component';
+import { MyMovieComponent } from './my-movie/my-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponent,
+    MyMovieComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ starter-code/src/app/routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { MyHomeComponent } from './my-home/my-home.component';
+import { MyMovieComponent } from './my-movie/my-movie.component';
+
+const routes: Routes = [
+  { path: '/home',  component: MyHomeComponent },
+  { path: '/movie/:id', component: MyMovieComponent }
+];
+
+export { routes };
